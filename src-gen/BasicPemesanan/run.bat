3a95f2ac5c8b44b26b50d265541ff785f490db2c
raissaputi <raissa.puti@gmail.com>
2025-05-02 20:38:59 +0700

    feat: hasil compile

---
+++ src-gen/BasicPemesanan/run.bat
@@ -0,0 +1,4 @@
+echo SELECT 'CREATE DATABASE siph_product_basicpemesanan' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'siph_product_basicpemesanan') \gexec | psql "postgresql://:@localhost"
+for %%G in (sql/*.sql) do psql -a -f sql/%%G "postgresql://:@localhost/siph_product_basicpemesanan"
+
+java -cp siph.product.basicpemesanan --module-path siph.product.basicpemesanan -m siph.product.basicpemesanan
